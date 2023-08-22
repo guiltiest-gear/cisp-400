55a2c48014673fe76a9853ff4fad25a6b2876185
Jade Fox <guiltier.gear@gmail.com>
2023-08-22 00:20:31 -0700

    Fix VectorFunctions.cpp code

--- m01-assignment/VectorFunctions.cpp
+++ m01-assignment/VectorFunctions.cpp
@@ -1,12 +1,31 @@
+// Name       : Jade Fox
+// ID         : 1722385
+// Program    : VectorFunctions.cpp
+// Date       : 8-22-2023
+// Description: Do the following:
+// 1. Declare a vector of integers of VECTOR_SIZE (=10) and each number ranges
+// between 1 and MAX_VALUE (=100)
+// 2. Populates the vector with random integers between the above values
+// 3. Sorts the vector in ascending order using the sort function available in
+// the algorithm library
+// 4. Displays the vector- use iterators to traverse the vector for display
+// 5. Reverses the vector in ascending order using the sort function available
+// 6. Displays the reversed vector
+// 7. Removes the vector element with least value
+// 8. Displays the reduced vector
+// 9. Calculates the average of the remaining vector values
+// 10. Display the results in a formatted way using 2 decimal places
 #include <algorithm>
+#include <iomanip>
 #include <numeric>
 
 #include "VectorHeader.h"
 
+using std::accumulate, std::cout, std::reverse, std::sort, std::vector,
+    std::setprecision, std::fixed, std::showpoint;
 // populate the vector with VECTOR_SIZE number of random integers of value
 // between 1 and 100 pass by ref ensures vector is returned with values to
 // calling program
-using std::accumulate, std::cout, std::reverse, std::sort, std::vector;
 
 void initializeVector(vector<int> &numbers) {
   // seed the random number
@@ -39,10 +58,13 @@ void displayVector(vector<int> numbers) {
 }
 
 // remove the number with the least value
-void removeLowest(vector<int> &numbers) { numbers.erase(numbers.begin()); }
+void removeLowest(vector<int> &numbers) { numbers.erase(numbers.end()); }
 
 // display the calculated average of the values
 void displayResults(double value) {
+  // Fix standard output to show 2 points of precision
+  cout << fixed << showpoint << setprecision(2);
+  // Display the value
   cout << "The average is " << value << '\n';
 }
 
